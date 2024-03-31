c39192f3fb3056312fcd85c8b246b226d805d795
amohii <shamchimohammad@gmail.com>
2024-03-31 15:48:28 +0300

    Users Repository & GraphQL CRUD - Part 1

--- chatter-backend/src/users/entities/user.entity.ts
+++ chatter-backend/src/users/entities/user.entity.ts
@@ -1,7 +1,17 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { AbstractEntity } from 'src/common/database/abstract.entity';
 
+@Schema({ versionKey: false })
 @ObjectType()
-export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class User extends AbstractEntity {
+  @Prop()
+  @Field()
+  email: string;
+
+  @Prop()
+  // we dont want to expose the password in the response so we dont add the @Field() decorator
+  password: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
