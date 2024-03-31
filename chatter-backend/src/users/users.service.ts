2ac4256d66cf1724c4960468014ad36ede2bf83f
amohii <shamchimohammad@gmail.com>
2024-03-31 18:43:59 +0300

    Validation Logging Pino

--- chatter-backend/src/users/users.service.ts
+++ chatter-backend/src/users/users.service.ts
@@ -1,29 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly userRepository: UsersRepository) {}
+  constructor(private readonly usersRepository: UsersRepository) {}
 
-  create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
+  async create(createUserInput: CreateUserInput) {
+    // for create user we reach out to the users Repository
+    return this.usersRepository.create({
+      ...createUserInput,
+      password: await this.hashPassword(createUserInput.password),
+    });
+    // and also because the user data now store in plain text we use the bcrypt library to hash the password
+  }
+  private async hashPassword(password: string) {
+    return bcrypt.hash(password, 10);
   }
 
   async findAll() {
-    return this.userRepository.find({});
+    return this.usersRepository.find({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  // We Change id:number to id:string because in mongodb the id is a string
+  async findOne(_id: string) {
+    return this.usersRepository.findOne({ _id });
   }
 
-  update(id: number, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`;
+  async update(_id: string, updateUserInput: UpdateUserInput) {
+    return this.usersRepository.findOneAndUpdate(
+      { _id },
+      {
+        $set: {
+          ...updateUserInput,
+          password: await this.hashPassword(updateUserInput.password),
+        },
+      },
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(_id: string) {
+    return this.usersRepository.findOneAndDelete({ _id });
   }
 }
